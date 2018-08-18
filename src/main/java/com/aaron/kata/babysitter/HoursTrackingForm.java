96878f5e58a8402e7d46f315c1161de878947b58
abenson91 <aaronbenson91@gmail.com>
2018-08-18 19:54:28 -0400

    1. Converted to Joda Time because comparing hours in my service layer will be easier. I knew I should have done this to start with idk why I didn't. Insert facepalm here

--- src/main/java/com/aaron/kata/babysitter/HoursTrackingForm.java
+++ src/main/java/com/aaron/kata/babysitter/HoursTrackingForm.java
@@ -1,4 +1,41 @@
 package com.aaron.kata.babysitter;
 
+import org.joda.time.DateTime;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotNull;
+
 public class HoursTrackingForm {
+
+    @NotNull(message = "Please enter a valid time in AM or PM")
+    @DateTimeFormat(pattern = "hh:mma")
+    private DateTime startTime;
+
+    @NotNull(message = "Please enter a valid time in AM or PM")
+    @DateTimeFormat(pattern = "hh:mma")
+    private DateTime endTime;
+
+    public DateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(DateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public DateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(DateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    @Override
+    public String toString() {
+        return "HoursTrackingForm{" +
+                "startTime=" + startTime +
+                ", endTime=" + endTime +
+                '}';
+    }
 }
